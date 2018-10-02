faed51509f7e091c80d91a2f4523f9950f9f0892
Andy Francisco Olarte Cardona <andyfranciscoolartecardona@Woloxs-MacBook-Pro-8.local>
2018-10-02 12:39:35 -0500

    tic tac toe avances

---
+++ training-react/tic-tac-toe/src/app/components/App/components/Game/components/Board/components/Square/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.scss';
+
+function Square(props) {
+  return (
+    <button className={styles.square} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+Square.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func
+};
+
+export default Square;
---
+++ training-react/tic-tac-toe/src/app/components/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Game from './components/Game';
+
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return <Game />;
+  }
+}
+
+export default App;
