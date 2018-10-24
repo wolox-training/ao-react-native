a68514d3fb247310c92455667b1462a177c81fb6
Andy Olarte <andy.olarte514@gmail.com>
2018-10-24 16:31:17 -0500

    correction login

--- training-react/tic-tac-toe/src/redux/login/actions.js
+++ training-react/tic-tac-toe/src/redux/login/actions.js
@@ -8,17 +8,21 @@ export const actionsTypes = {
 
 const actionCreators = {
   authUser: userData => async dispatch => {
+    let token;
     const params = { username: userData.email, password: userData.password };
     const response = await loginService.authUser(params);
-    const token = response.data.id;
     if (response.ok) {
+      token = response.data.id;
       localStorage.setItem('userToken', token);
       localStorage.setItem('userId', response.data.userId);
       loginService.setToken(token);
     }
     dispatch({
-      type: response.data.error ? actionsTypes.HAS_ERROR_USER : actionsTypes.AUTH_USER,
-      payload: response.data.error ? response.data.error.message : token
+      type:
+        response.status === 200 || response.status === 201
+          ? actionsTypes.AUTH_USER
+          : actionsTypes.HAS_ERROR_USER,
+      payload: response.status === 200 || response.status === 201 ? token : 'LOGIN FAILED'
     });
   },
   signOutUser: () => dispatch => {
