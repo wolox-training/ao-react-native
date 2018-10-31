c5a229ba3a3aa598d55bb7df02e8c839617d896e
Andy Olarte <andy.olarte514@gmail.com>
2018-10-31 16:04:19 -0500

    correction of comments made by Henry

--- training-react/tic-tac-toe/src/app/components/Loader/index.js
+++ training-react/tic-tac-toe/src/app/components/Loader/index.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import style from './styles.scss';
 
 function Loader(WrappedComponent) {
-  class Comp extends Component {
-    render() {
-      const { loading, ...props } = this.props;
-      return loading ? (
-        <div className={style.containerLoader}>
-          <div className={style.loader} />
-        </div>
-      ) : (
-        <WrappedComponent {...props} />
-      );
-    }
+  function Comp({ loading, ...props }) {
+    return loading ? (
+      <div className={style.containerLoader}>
+        <div className={style.loader} />
+      </div>
+    ) : (
+      <WrappedComponent {...props} />
+    );
   }
 
   Comp.propTypes = {
-    loading: PropTypes.bool.isRequired
+    loading: PropTypes.bool
   };
 
   return Comp;
